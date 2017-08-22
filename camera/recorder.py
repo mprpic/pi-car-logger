b982354f85df17af3a6e1a41e23a550ea888109c
Martin Prpič <martin.prpic@gmail.com>
2017-08-22 21:32:42 +0200

    Fix filename generation
    Timestamps in file name were useless since we can get them from
    file attributes.

--- camera/recorder.py
+++ camera/recorder.py
@@ -1,32 +1,40 @@
+import os
 import glob
 import signal
 from itertools import cycle, dropwhile
-from datetime import datetime
 from picamera import PiCamera
 
 STOP_RECORDING = False
 CAMERA_RES = '720p'
 RECORDING_LENGTH = 10  # Length of individual videos
 MAX_NUM_FILES = 2000   # Altogether around 5.55 hours of video at max
+FILE_SUFFIX = '.h264'
 
 
 def generate_filenames():
-    # Filename syntax used: NNNN_YYYY-MM-DD_HH-MM-SS.h264
-    # where NNNN cycles from 0001 to MAX_NUM_FILES
     existing_files = glob.glob('*.h264')
+    working_dir = os.path.dirname(os.path.realpath(__file__))
 
     if not existing_files:
         count_from = 1
     else:
-        count_from = max(int(f.split('_')[0].lstrip('0')) for f in existing_files)
-        if count_from == MAX_NUM_FILES:
+        newest_file = max(existing_files, key=os.path.getctime)
+        count_from = int(newest_file.rstrip(FILE_SUFFIX).lstrip('0'))
+
+        if count_from >= MAX_NUM_FILES:
             count_from = 1
+        else:
+            count_from += 1  # Bump to next available ID after latest file
 
     iterable = dropwhile(lambda x: x < count_from, cycle(xrange(1, MAX_NUM_FILES + 1)))
     for file_id in iterable:
         if STOP_RECORDING:
             break
-        yield datetime.now().strftime('{}_%Y-%m-%d_%H-%M-%S.h264'.format(str(file_id).zfill(4)))
+
+        filename = str(file_id).zfill(4) + FILE_SUFFIX
+        filename = os.path.join(working_dir, filename)
+
+        yield filename
 
 
 def quit_recorder(*args):
@@ -35,8 +43,8 @@ def quit_recorder(*args):
 
 
 def record(camera):
-    print('Started recording...')
-    for i in camera.record_sequence(generate_filenames()):
+    print('Starting recording...')
+    for _ in camera.record_sequence(generate_filenames()):
         camera.wait_recording(RECORDING_LENGTH)
 
 
